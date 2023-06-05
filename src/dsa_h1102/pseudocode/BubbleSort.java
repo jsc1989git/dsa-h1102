c81851e3458d709339703566fbe21ab919293f62
jsc <jsc1989@gmail.com>
2023-06-06 02:43:19 +0800

    Complete BubbleSort pseudocode

--- src/dsa_h1102/pseudocode/BubbleSort.java
+++ src/dsa_h1102/pseudocode/BubbleSort.java
@@ -16,5 +16,27 @@ public class BubbleSort {
                     arr[j] = arr[j+1]
                     arr[j+1] = temp
 
-    //Return
+        //Return the sorted array
+        return arr
+
+        //Function to print an array of integers to the console
+        function printArray(arr):
+            //Get the length of the input array
+            n = length(arr)
+
+            //Loop to print each element of the array to the console
+            for i = 0 to n-1
+                print(arr[i])
+
+        //Main program
+        function main():
+            //Input array of integers to be sorted
+            arr = [5, 3, 8, 4, 2]
+
+            //Sort the input array using the bubble sort algorithm
+            sortedArr = bubbleSort(arr)
+
+            //Print the sorted array to the console
+            print("Sorted array.")
+            printArr(sortedArr)
 }
