c0475195a1e0de4d9b06e5c5dd81b91696ef33bc
choi <jsc1989@proton.me>
2023-06-05 10:55:30 +0800

    Create Stack3Elements class

---
+++ src/dsa_h1102/Stack3Elements.java
@@ -0,0 +1,24 @@
+package dsa_h1102;
+
+import java.util.Stack;
+
+public class Stack3Elements {
+    public static void main(String[] args) {
+        //Create a stack of strings
+        Stack<String> stack = new Stack<String>();
+
+        //Push three elements onto the stack
+        stack.push("Alice");
+        stack.push("Bob");
+        stack.push("Charlie");
+
+        //Pop the pushed elements and print them using LIFO order
+        while (!stack.isEmpty()) {
+            String popped = stack.pop();
+            System.out.println(popped);
+        }
+
+        //Check if the stack is empty
+        System.out.println(stack.isEmpty()); //should return true
+    }
+}
